1a52614b8dfc983494772cc6b9e9aa5a3fe10937
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-09-11 03:11:56 +0800

    finished proto

---
+++ src/proto/proto.js
@@ -0,0 +1,20 @@
+//false  +
+//false+
+//true+
+//false+
+//false+
+//true-
+// true+
+//false-
+//prototype.Function+
+//prototype.Number+
+// false +
+// false+
+//true+
+//false+
+//false+
+//false+
+//true+
+//true+
+//prototype.Function+
+//prototype.Number+
