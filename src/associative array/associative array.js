bf1946d28204ec786b8b00e70449874528129454
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-10-25 14:24:31 +0800

    finished lesson about associative array

---
+++ src/associative array/associative array.js
@@ -0,0 +1,56 @@
+let car=["Nisan", "Легковой", 2015, "Black", 3000, false]
+console.log()
+let car_2={
+    brand:"Nisan",
+    type:"Легковой",
+    year:2015,
+    color:"Black",
+    repaired:false
+}
+
+// const hotel = {
+//     'name': 'U doma',
+//     'stars': 3,
+//     'abv': function () {
+//         console.log(this.name + ' best hotel. Stars: ' + this.stars);
+//     }
+// };
+// console.log(hotel.name);
+// console.log(hotel["name"]);
+// hotel.abv();
+// // let mas={
+// //     "one":4,
+// //     "two":33,
+// //     "name":"Alex",
+// //     "age":44,
+// //     "bool":true,
+// //     "0":0,
+// //     "1":1,
+// //     "2":2,
+// // }
+// // console.log(mas['one']);
+// // console.log(mas[0]);
+// // console.log(mas[1]);
+// // console.log(mas[2]);
+// //
+// //
+// //
+// // const a = {
+// //     'a': 5,
+// //     'b': 'Hello',
+// //     'z2': 'Hi',
+// //     y43: 1999,
+// //     'villa de': 3040
+// // };
+// // console.log(a);
+// // delete a.a
+// // console.log(a.a);
+// // console.log(a['villa de']);
+// // a.yyyy = 555;
+// //
+// // console.log(a);
+// // let out = '';
+// // for (let key in a) {
+// //     out += a[key]+" ";
+// // }
+// // console.log(out);
