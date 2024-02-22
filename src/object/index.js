867bea4c1b63687e99c8daf643f91ae6f37a5534
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2024-02-22 21:20:59 +0800

    finished lesson about object

--- src/object/index.js
+++ src/object/index.js
@@ -1,11 +1,47 @@
+const person = Object.create({}, {
+  name: {
+    value: 'Dimych',
+    enumerable: true,
+    writable: true,
+    configurable: true
 
+  },
+  birthYear: {
+    value: 1993,
+    enumerable: false,
+    writable: false,
+    configurable: false
+  },
+  age: {
+    // value: new Date().getFullYear() - 1993,
+    enumerable: true,
+    get() {
+      return new Date().getFullYear() - this.birthYear;
+    },
+    set() {
+    },
+  }, greetings() {
+    console.log('hello');
+  }
+
+});
+
+// const person = {
+//   name: 'Dimych',
+//   age: 32,
+//   isMarried: true
+// };
+// delete person.name;
+for (let key in person) {
+  console.log('ker', key, person[key]);
+}
+console.log(person);
 // let obj1 = {
 //   name: 'Demych',
 // };
 // let newObj = Object.assign({}, obj1);
 //
 // console.log(Object.is(newObj, obj1));
-
 
 
 // const userInfo = {
@@ -25,3 +61,5 @@
 //   isMarried: true
 // };
 // console.log(Object.is(person, person2));
+
+
