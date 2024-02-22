01cac0991c3046da29f5249d6698b366a161c940
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2024-02-22 15:02:49 +0800

    finished lesson about function and object

---
+++ src/function/index.js
@@ -0,0 +1,32 @@
+"use strict";
+function createIncrement() {
+  let count = 0;
+
+  function increment() {
+    count++;
+  }
+
+  let message = `Count is ${count}`;
+
+  function log() {
+    console.log(message);
+  }
+
+  return [increment, log];
+}
+
+const [increment, log] = createIncrement();
+increment();
+increment();
+increment();
+log();
+
+
+function returnArray() {
+    return [
+      'a',
+      'b'
+    ];
+}
+
+console.log(returnArray());
---
+++ src/object/index.js
@@ -0,0 +1,27 @@
+
+// let obj1 = {
+//   name: 'Demych',
+// };
+// let newObj = Object.assign({}, obj1);
+//
+// console.log(Object.is(newObj, obj1));
+
+
+
+// const userInfo = {
+//   name: 'Dimych',
+//   age: 32,
+// };
+
+
+// const person = {
+//   name: 'Dimych',
+//   age: 32,
+//   isMarried: true
+// };
+// const person2 = {
+//   name: 'Dimych',
+//   age: 32,
+//   isMarried: true
+// };
+// console.log(Object.is(person, person2));
