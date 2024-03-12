7eaf62fd8a5b39b85df47c7a72ee6401b394e54e
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2024-03-12 14:12:22 +0800

    finished  little lessons about closure

---
+++ src/closure/index.js
@@ -0,0 +1,14 @@
+const x = 1;
+const logToConsole = function () {
+  let x=2
+  console.log(x);
+};
+logToConsole()
+
+// const x = 1;
+// const logToConsole = function () {
+//   debugger
+//   const i = 'hi';
+//   console.log(i);
+// };
+// logToConsole()
